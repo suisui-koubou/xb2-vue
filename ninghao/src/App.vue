6083c33f5840a8e86935e5235888003f0d08863c
randoruf <masswie@foxmail.com>
2024-09-28 22:48:31 +0800

    day 06 - animation and animate.css

--- ninghao/src/App.vue
+++ ninghao/src/App.vue
@@ -8,7 +8,10 @@
       <div class="card__content">
         <!-- v-if使得普通组件失去动画效果，如果想做切入和切出的动画，可以用 transition 包装，
          移除和加入会出现 v-enter-active v-enter-to 等类(自动添加的，也可自己定义) -->
-        <transition name="pulse">
+        <transition name="tree"
+          enter-active-class="animate__animated animate__flip"
+          leave-active-class="animate__animated animate__fadeOutTopLeft"
+        >
           <div v-if="isActive" :class="['emoji', { pulse : !isActive}]">🌲</div>
         </transition> 
       </div>
@@ -36,4 +39,5 @@ export default defineComponent({
 <style>
 @import './styles/app.css';
 @import './styles/card.css';
+@import './styles/animate.css';
 </style>
