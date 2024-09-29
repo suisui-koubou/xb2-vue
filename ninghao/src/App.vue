a3a45a80e02779fed05c2c88bbfb2c94361d35d5
randoruf <masswie@foxmail.com>
2024-09-29 16:30:49 +0800

    day 06 - lodash

--- ninghao/src/App.vue
+++ ninghao/src/App.vue
@@ -6,18 +6,15 @@
         <div class="card-subtitle"> Transition & Animation </div>
       </div>
       <div class="card__content">
-        <!-- v-if使得普通组件失去动画效果，如果想做切入和切出的动画，可以用 transition 包装，
-         移除和加入会出现 v-enter-active v-enter-to 等类(自动添加的，也可自己定义) -->
-        <transition name="tree"
-          enter-active-class="animate__animated animate__flip"
-          leave-active-class="animate__animated animate__fadeOutTopLeft"
-          mode="out-in"
-        >
-          <component :is="currentEmoji"></component>
-        </transition> 
+        <transition-group>
+          <!-- :key 是用来操作元素的，所以不建议key绑定 index -->
+          <div class="emoji" v-for="emoji in emojiList" :key="emoji"> 
+            {{ emoji }}
+          </div>
+        </transition-group>
       </div>
       <div class="card__action">
-        <button @click="isActive = !isActive" :class="{ 'active': isActive }">请按这里</button>
+        <button @click="shuffle" :class="{ 'active': isActive }">请按这里</button>
       </div>
     </div>
     <div class="status"><small>isActive: {{ isActive }}</small></div>
@@ -26,26 +23,22 @@
 
 <script>
 import { defineComponent } from 'vue';
-import womanPoliceEmoji from './components/woman-police-emoji.vue';
-import momoEmoji from './components/momo-emoji.vue';
+import _ from 'lodash'; 
+
 
 export default defineComponent({
   data() {
     return {
       name: '宁浩网',  
-      isActive: false
+      isActive: false,
+      emojiList: ['♠️', '♣️', '♥️', '♦️']
     }
-  }, 
+  },
 
-  computed: {
-    currentEmoji(){
-      return this.isActive ? 'momoEmoji' : 'womanPoliceEmoji'; 
+  methods: {
+    shuffle() {
+      this.emojiList = _.shuffle(this.emojiList); 
     }
-  },  
-
-  components: {
-    womanPoliceEmoji, 
-    momoEmoji
   }
 });
 </script>
