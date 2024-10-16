bf296b63b8784b81382b1d5b55ce8ac9861b0822
randoruf <masswie@foxmail.com>
2024-10-16 11:18:17 +0800

    day 2 - cors dependencies

--- ninghao/src/App.vue
+++ ninghao/src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    <h3>Hello World!</h3>
+    <h3>{{ greeting }}</h3>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
+import axios from 'axios'; 
 
 export default defineComponent({
   name: 'App',
+
+  data() {
+    return {
+      greeting: 'Hello World!', 
+    }
+  }, 
+
+  mounted(){
+    axios.get('http://localhost:3000/posts').then(res=>{
+      this.greeting = res.data[0].title;
+      
+		}); 
+  }
 });
 </script>
 
