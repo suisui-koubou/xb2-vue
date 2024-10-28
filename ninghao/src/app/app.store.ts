d46fc5da89036378747c29e70f6160f5c259ac08
randoruf <masswie@foxmail.com>
2024-10-28 23:44:17 +0800

    day 1 - the structure of store

--- ninghao/src/app/app.store.ts
+++ ninghao/src/app/app.store.ts
@@ -5,6 +5,7 @@ import { createStore } from "vuex";
  * create a store 
  * (每一个文件夹可以当作是一个模块，比如 app, user, post, comment)
  * (每一个模块下有单独的Store, 可以更有逻辑和层次)
+ * (仔细在 console.log 查看 Store 对象的结构)
  */
 const store = createStore({
     state: {
