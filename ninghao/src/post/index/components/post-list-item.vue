7b2a7fd793311842c4718728851827dafdfd9634
randoruf <masswie@foxmail.com>
2024-10-30 11:52:59 +0800

    day 2 - component properties

--- ninghao/src/post/index/components/post-list-item.vue
+++ ninghao/src/post/index/components/post-list-item.vue
@@ -7,7 +7,8 @@
 <script>
 import { defineComponent } from 'vue';
 
-//  
+// 在 post-list.vue 文件的 :item='post' 会将每一次的post交给item属性。
+// 在这里就可以直接使用item属性。
 export default defineComponent({
     props: {
         item: Object
