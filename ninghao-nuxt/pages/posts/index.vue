a95970b909473367969173565a58a6f47184fcbc
randoruf <masswie@foxmail.com>
2024-11-25 12:28:04 +0800

    #52 - nuxt posts

--- ninghao-nuxt/pages/posts/index.vue
+++ ninghao-nuxt/pages/posts/index.vue
@@ -1,6 +1,14 @@
 <template>
     <div>
         <h1>内容列表</h1>
-        
+        <div v-for="post in posts" :key="post.id">
+            <div>{{ post.title }}</div>
+            <div>- <small>{{ post.user.name }}</small></div>
+        </div>
     </div>
 </template>
+
+<script setup>
+const posts = await $fetch('http://localhost:3001/posts'); 
+console.log(posts); 
+</script>
