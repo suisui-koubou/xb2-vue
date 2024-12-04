ae30c5cbe8ed680b156a89707f7de7bfcf82cce2
randoruf <masswie@foxmail.com>
2024-12-04 23:57:05 +0800

    #51 - nuxt useAsyncData and

--- ninghao-nuxt/pages/posts/index.vue
+++ ninghao-nuxt/pages/posts/index.vue
@@ -1,14 +1,40 @@
 <template>
     <div>
         <h1>内容列表</h1>
-        <div v-for="post in posts" :key="post.id">
-            <div>{{ post.title }}</div>
-            <div>- <small>{{ post.user.name }}</small></div>
+        <div>
+            <button @click="backward">上一页</button>
+            <button @click="forward">下一页</button>
+        </div>
+        <div v-if="pending">加载中...</div>
+        <div v-if="posts !== null">
+            <div v-for="post in posts" :key="post.id">
+                <div>{{ post.title }}</div>
+                <div>- <small>{{ post.user.name }}</small></div>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
-const posts = await $fetch('http://localhost:3001/posts'); 
-console.log(posts); 
+
+const page = ref(1); // 组合模式下的响应数据
+const backward = () => {
+    page.value--; 
+    refresh(); 
+}; 
+const forward = () => {
+    page.value++;
+    refresh(); 
+}; 
+
+const {
+    data:posts, // 获取的数据data重命名为posts
+    pending, 
+    refresh, 
+    error
+} = await useAsyncData('posts', () => 
+    $fetch(`http://localhost:3001/posts?page=${page.value}`),
+    { lazy: true } // 惰性加载(未加载时不会阻止导航)。
+); 
+
 </script>
