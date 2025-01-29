021a5a881fab2cfcc951ae29baac07ef08421c33
randoruf <masswie@foxmail.com>
2025-01-29 16:35:19 +0800

    #65 - next.js

---
+++ ninghao-next/src/app/about/page.tsx
@@ -0,0 +1,15 @@
+
+const Page = () => {
+    return (
+      <div>
+        <main>
+          <h1>关于</h1>
+          <div>宁皓网创建于2011年。</div>
+        </main>
+        <footer>Footer</footer>
+      </div>
+    );
+  }
+  
+  
+export default Page;
